5974fb6dddb3ebb8360497ff4207dab5cecb11a6
Erik Peldan <regnskog@gmail.com>
2013-03-18 23:12:03 +0100

    started rewrite of chess logic..

--- chess.hs
+++ chess.hs
@@ -10,206 +10,28 @@ import Game
 import Piece
 import MonadOps
 
-cantReach d s g@(Game b p) =
-    maybe (noPieceStr s) (pieceCantReach d) (b !!! s)
+data Move = Move Square Square
 
-noPieceStr s = "No piece at " ++ squareToString s
+makeMove (Game b p) (Move s d) = do
+    let maybePiece = b !!! d
+    when (isNothing maybePiece) $ fail "No piece to move"
+    piece <- maybePiece
+    let Piece t c = piece
+    let reachables = reachableSquaresByOfficerAt t s
+    unless (d `elem` concat reachables) $ fail "Cannot reach destination"
+    dir <- find (elem d) reachables
+    ix <- findIndex (d ==) dir
+    let isOccupied = not. isNothing
+    hit <- maybe (length dir) (findIndex isOccupied) dir
+    --TODO check for same-colored piece
+    unless (ix <= hit) $ fail "Cannot reach destination"
+    --TODO replace
 
-pieceTypeCantReach d t =
-    pieceTypeToString t ++ " can't reach" ++ squareToString d
-
-pieceCantReach d (Piece t _) = 
-    pieceTypeCantReach d t
-
-castlingAssociations = 
-    let square f r = Square (File f) (Rank r)
-     in [
-        (square 'e' 1, whitesRights),
-        (square 'h' 1, whitesRight' Kingside),
-        (square 'a' 1, whitesRight' Queenside),
-        (square 'e' 8, blacksRights),
-        (square 'h' 8, blacksRight' Kingside),
-        (square 'a' 8, blacksRight' Queenside)
-        ]
-                
-colorAfterMove _ _ (Game _ p) = otherColor (whoseMove p)
-rightsAfterMove d s (Game _ p) =
-    let oldRights = castlingRights p
-        rightsToRemove s = maybe [] id (lookup s castlingAssociations)
-     in
-        (oldRights \\ rightsToRemove s) \\ rightsToRemove d
-
-enPassantAfterMove d s g@(Game _ p) =
-    let up1 = Chess.relUp g
-        rank' = fromEnum . rank
-     in
-        if file d == file s && abs (rank' s - rank' d) == 2 then
-            Just (fromSquare s up1) else
-            Nothing
-
-isPawn p = pieceType p == Pawn
-
-halfMovesAfterMove d s g@(Game b p) =
-    let isCapture = Nothing /= b !!! d
-        isPawnMove = maybe False isPawn (b !!! s)
-     in
-        if isCapture || isPawnMove
-        then
-            0
-        else
-            halfMoveNumber p + 1
-
-moveNumberAfterMove _ _ (Game _ p) = moveNumber p + 1
-
-propertiesAfterMove d s g@(Game _ p) =
-    GameProperties {
-        whoseMove      = colorAfterMove d s g,
-        castlingRights = rightsAfterMove d s g,
-        enPassantSquare = enPassantAfterMove d s g,
-        halfMoveNumber = halfMovesAfterMove d s g,
-        moveNumber = moveNumberAfterMove d s g
-        }
-
-isPromotionMove d s (Game b p) =
-    let isPawnMove = maybe False isPawn (b !!! s)
-        c = whoseMove p
-     in
-        isPawnMove && rank d == lastRank c
-
-noPromotion p =
-    if p /= Nothing
-    then
-        fail "Can't promote"
-    else
-        return
-
-promoteTo Nothing d (Game _ ps) = fail "Missing promotion"
-
-promoteTo (Just pt) d (Game _ ps) = 
-    let p = Piece (Officer pt) (whoseMove ps)
-    in  return . replace' d (Just p) 
-
-promoCont d s g promo =
-    if isPromotionMove d s g
-        then
-            promoteTo promo d g
-        else
-            noPromotion promo
-
-boardAfterMove d s g@(Game b _) = move' s d b 
-
-canMove d s g promo = isLegal (gameAfterMove d s g promo)
-
-isFriendlyColor (Game _ p) = (whoseMove p ==)
-isFriendlyPiece :: Game -> Piece -> Bool
-isFriendlyPiece g = isFriendlyColor g . color
-isFriendlyPieceAt :: Square -> Game -> Bool
-isFriendlyPieceAt s g@(Game b _) = 
-    maybe False (isFriendlyPiece g) (b !!! s)
-
-findFriendlies g@(Game b p) = 
-    let isFriendly :: Maybe Piece -> Bool
-        isFriendly = maybe False (isFriendlyPiece g)
-     in
-        map toEnum (findIndices isFriendly b)
-
-isLegal :: Game -> Bool
-isLegal g = case legalize g of
-    Left _ -> False
-    _ -> True
-
-data ChessError = MissingKing | KingNotSafe | PawnsOnLastRank
-chessErrorToString MissingKing = "Enemy king is missing!"
-chessErrorToString KingNotSafe = "Enemy king can be captured!"
-chessErrorToString PawnsOnLastRank = "There are pawns on the last rank!"
-
-legalize g = do
-    unless (Nothing /= enemyKingSquare g) (Left MissingKing)
-    unless (kingIsSafe g) (Left KingNotSafe)
-    unless (noPawnAtLastRank g) (Left PawnsOnLastRank)
-
-noPawnAtLastRank g@(Game b _) = not $ any pawnAt lastRanks
-    where
-        pawnAt s = maybe False ((Pawn ==) . pieceType) (b !!! s)
-        lastRanks = [Square (File 'a') (Rank 1)..Square (File 'h') (Rank 1)] ++ 
-            [Square (File 'a') (Rank 8)..Square (File 'h') (Rank 8)]
-
-enemyKingPiece (Game _ p) = Piece (Officer King) (whoIsNotMoving p)
-
-enemyKingSquare :: Game -> Maybe Square
-enemyKingSquare g@(Game b p) =
-    let rightPiece :: Maybe Piece -> Bool
-        rightPiece =
-            maybe False (== enemyKingPiece g)
-     in toEnum <$> findIndex rightPiece b
-
-takableFromSquare :: Square -> Game -> [Square]
-takableFromSquare s g = 
-    let 
-        takeOK = flip canTake g
-        takables = pieceTakables s g
-     in 
-        filter takeOK . concat $ firstNonMovable g <$> takables
-
-firstNonMovable g = 
-    let moveOK = flip canMoveTo g 
-    in  take 1 . dropWhile moveOK
-
-pieceTakables :: Square -> Game -> [[Square]]
-pieceTakables s g@(Game b _) =
-    case b !!! s of
-        Nothing -> error $ nothingAt s
-        Just p -> pieceTakables' p s g 
-
-pieceTakables' :: Piece -> Square -> Game -> [[Square]]
-pieceTakables' p s g@(Game b _) =
-    case p of
-        Piece Pawn _ -> pawnTakables s g
-        Piece (Officer o) _ -> officerMovables s o
-
-kingIsSafe g@(Game b p) = 
-    let threatens d s = d `elem` takableFromSquare s g
-        kingSquare = fromJust $ enemyKingSquare g
-        friendlies = findFriendlies g
-     in
-        threatens kingSquare `any` friendlies
-
--- Tries to perform the move WITHOUT validating
-gameAfterMove d s g@(Game b _) Nothing =
-    Game b' p'
-    where
-        b' = boardAfterMove d s g
-        p' = propertiesAfterMove d s g
-
-gameAfterMove d s g@(Game b _) mp =
-    let Game b' p' = gameAfterMove d s g Nothing
-     in Game (replace' d mp b') p'
-
-coloredPieces (Game b p) c =
-    let isRightColoredPiece (Piece t c') = c == c'
-     in
-        findIndices (maybe False isRightColoredPiece) b
-
-nothingAt :: Square -> String
-nothingAt s = "Nothing at " ++ squareToString s
-
-pieceAt s b = case b !!! s of
-    Nothing -> error $ nothingAt s 
-    Just p  -> p
-
-officerMovables :: Square -> OfficerType -> [[Square]]
-officerMovables s Bishop =
-    let upright = up 1 >=> right 1
-        upleft  = up 1 >=> left 1
-        downright = down 1 >=> right 1
-        downleft  = down 1 >=> left 1
-     in
-        sequence' s <$> [upright, upleft, downright, downleft]
-
-officerMovables s Rook   = sequence' s <$> [up 1, left 1, down 1, right 1]
-officerMovables s Queen  = concat . officerMovables s <$> [Rook, Bishop]
-officerMovables s King   = take 1 <$> officerMovables s Queen
-officerMovables s Knight = fromSquare' s <$> knightJumps
+reachableSquaresByOfficerAt Rook s  = sequence' s <$> [up 1, left 1, down 1, right 1]
+reachableSquaresByOfficerAt Queen s = concat . flip reachableSquaresByOfficerAt s <$>
+    [Rook, Bishop]
+reachableSquaresByOfficerAt King s  = take 1 <$> reachableSquaresByOfficerAt Queen s
+reachableSquaresByOfficerAt Knight s = fromSquare' s <$> knightJumps
 
 knightJumps = 
     let long = [up, right] <*> [2,-2]
@@ -217,39 +39,5 @@ knightJumps =
      in
         zipWith (>=>) long short
 
-canTake :: Square -> Game -> Bool
-canTake d (Game b p) =
-    let 
-        hasOppositeColor = (/= whoseMove p) . color
-     in
-        maybe False hasOppositeColor (b !!! d)
-
-canTakeEP d (Game b p) = 
-    case enPassantSquare p of
-        Nothing -> False
-        Just s  -> Nothing == b !!! d && d == s
-
-canMoveTo :: Square -> Game -> Bool
-canMoveTo d (Game b _) =
-    Nothing == b !!! d
-
-friendlyAt d = 
-    concat [
-        "Can't move to ",
-        squareToString d,
-        ". Square occupied by friendly piece!"
-    ]
-
 relUp (Game _ p) = Square.relUp 1 (whoseMove p)
 
-pawnTakables s g@(Game _ p) =
-    let up1 = Chess.relUp g
-        left1 = left 1
-        right1 = right 1
-     in
-        fromSquare' s <$> [up1 >=> left1, up1 >=> right1]
-
-pawnMovables s g@(Game _ p) = 
-    let up1 = Chess.relUp g 
-     in 
-        fromSquare s <$> [up1, up1 >=> up1]
