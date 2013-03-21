d21ccc817c456f4e5c0c866e553a2eaf2e24f4df
Erik Peldan <regnskog@gmail.com>
2013-03-21 23:29:19 +0100

    range module WIP

--- range.hs
+++ range.hs
@@ -1,4 +1,4 @@
-module Range where
+module Range (Range, range) where
 import Internals (MovingPiece)
 import Square (SquareSeries)
 import Piece
@@ -6,3 +6,14 @@ import Piece
 type Range = [SquareSeries]
 
 range mp = []
+
+range' Pawn White s = []
+
+data RangeType = Takes | Moves
+pawnRange c s Moves = []
+pawnMoves White 2 Moves = [above, above >=> above]
+pawnMoves White _ Moves = [above]
+pawnMoves Black 7 Moves = [below, below >=> below]
+pawnMoves Black _ Moves = [below]
+pawnMoves White _ Takes = [above >=> leftOf, above >=> rightOf]
+pawnMoves Black _ Takes = [below >=> leftOf, below >=> rightOf]
