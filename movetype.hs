30b10babe3e8938e4d1b3efeb4b4d4fd54a024bf
Erik Peldan <regnskog@gmail.com>
2013-03-30 13:32:21 +0100

    new separate move type module

---
+++ movetype.hs
@@ -0,0 +1,5 @@
+module MoveType (
+    MoveType(..),
+    ) where
+
+data MoveType = Takes | Moves deriving (Show, Eq)
--- movingpiece.hs
+++ movingpiece.hs
@@ -2,13 +2,17 @@ module MovingPiece (
     MovingPiece(..),
     movingPiece,
     piece,
+    pieceType,
+    color,
+    MovingPiece.range,
     )where
 import Data.Maybe (fromJust)
 import Control.Monad.Error (throwError)
 
 import Square (Square)
-import Piece (
+import qualified Piece (
     Piece,
+    pieceType,
     color,
     verifyHasColor,
     )
@@ -22,6 +26,11 @@ import ErrorMonad (
     ErrorMonad,
     Reason(NoPieceToMove),
     )
+import Range(
+    Range,
+    range,
+    )
+import MoveType(MoveType)
 
 data MovingPiece = PieceFromPosition {
     position :: Position,
@@ -32,7 +41,15 @@ movingPiece :: Position -> Square -> ErrorMonad MovingPiece
 movingPiece p s = do
     let c = whoseTurn p
     let x = p `readSquare` s
-    maybe (throwError NoPieceToMove) (verifyHasColor c) x
+    maybe (throwError NoPieceToMove) (Piece.verifyHasColor c) x
     return (PieceFromPosition p s)
 
 piece mp = fromJust $ position mp `readSquare` MovingPiece.square mp
+color = Piece.color . piece
+pieceType = Piece.pieceType . piece
+
+range :: MovingPiece -> MoveType -> Range
+range mp = Range.range 
+    (color mp)
+    (pieceType mp)
+    (square mp)
