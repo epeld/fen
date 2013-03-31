d021c7f7cc9fbd40db53b9b181dad025426facd8
Erik Peldan <regnskog@gmail.com>
2013-03-31 13:17:31 +0200

    simplified module

--- move.hs
+++ move.hs
@@ -1,4 +1,7 @@
 module Move (Move, move) where
+import Prelude hiding (elem)
+import Data.Maybe (maybe, fromJust)
+import Control.Monad.Error ( throwError)
 
 import Square (
     Square, rank
@@ -27,44 +30,33 @@ import MovingPiece (
     )
 import ProjectedRange (
     projectedRange,
-    elem
+    elem,
+    whichMoveType,
     )
 import MoveType (
     MoveType(..)
     )
 
-import Prelude hiding (elem)
-import Data.Maybe ( fromJust)
-import Control.Monad.Error ( throwError)
-
 data Move = Move {
     movingPiece :: MovingPiece,
     destination :: Square,
+    moveType :: MoveType,
     promotion :: Maybe Promotion
     }
-data ClassifiedMove = Standard Move | Capturing Move
 
-move :: Position -> Square -> Square -> Maybe Promotion -> ErrorMonad ClassifiedMove
+move :: Position -> Square -> Square -> Maybe Promotion -> ErrorMonad Move
 move p s d pr = do
     mp <- MovingPiece.movingPiece p s
     move' mp d pr
 
-move' :: MovingPiece -> Square -> Maybe Promotion -> ErrorMonad ClassifiedMove
+move' :: MovingPiece -> Square -> Maybe Promotion -> ErrorMonad Move
 move' mp d pr = do
-    mv <- classify $ Move mp d pr
+    mt <- identifyMoveType mp d
+    let mv = Move mp d mt pr
     verifyPromotion (rank d) (color mp) (pieceType mp) pr
     return mv
 
-classify :: Move -> ErrorMonad ClassifiedMove
-classify m = case (moveable m, takeable m) of
-    (True, True)  -> error "classify confused :("
-    (True, False) -> return $ Standard m
-    (False, False) -> throwError NotInRange
-    (False, True) -> return $ Capturing m
-
-inRange mt m = destination m `elem` projectedRange (Move.movingPiece m) mt
-moveable = inRange Moves
-takeable = inRange Takes
+identifyMoveType mp d = maybe (throwError NotInRange) return (whichMoveType mp d)
 
 verifyPromotion 8 White Pawn Nothing = throwError LastRankPromote
 verifyPromotion 1 Black Pawn Nothing = throwError LastRankPromote
