cfe16a68c7b4adac7e9fade06cc5d5222cc33683
Erik Peldan <regnskog@gmail.com>
2013-04-28 12:03:21 +0200

    rework projection logic and structure

--- position.hs
+++ position.hs
@@ -1,23 +1,10 @@
-module Position(
-    Position(..),
-    Position.readSquare,
-    Promotion,
-    friendlyColor,
-    enemyColor,
-    ) where
+module Position( Position(..), Position.readSquare, Promotion, 
+                 friendlyColor, enemyColor, containsFriendlyPiece,
+                 containsEnemyPiece, isCapturableSquare) where
 import Square (Square)
-import Piece (
-    Piece,
-    OfficerType,
-    )
-import Color (
-    Color,
-    invert,
-    )
-import Board (
-    Board,
-    readSquare,
-    )
+import Piece ( Piece, OfficerType, color,)
+import Color ( Color, invert,)
+import Board ( Board, readSquare,)
 import CastlingRight (Right)
 
 data Position = Position {
@@ -31,7 +18,26 @@ data Position = Position {
 
 type Promotion = OfficerType
 
+isCapturableSquare :: Position -> Square -> Bool
+isCapturableSquare p s = if isPassantSquare p s
+    then True
+    else containsEnemyPiece p s
+
+isPassantSquare p s = enPassant p == Just s
+
+containsPieceSatisfying criterion p s = maybe False criterion piece
+    where piece = Position.readSquare p s
+
+containsEnemyPiece p = containsPieceSatisfying (hasEnemyColor p) p
+containsFriendlyPiece p = containsPieceSatisfying (hasFriendlyColor p) p
+
 readSquare p s = Board.readSquare (board p) s
+
+hasEnemyColor :: Position -> Piece -> Bool
+hasEnemyColor p pc = color pc == enemyColor p
+
+hasFriendlyColor :: Position -> Piece -> Bool
+hasFriendlyColor p = not. hasEnemyColor p
 
 enemyColor = invert . whoseTurn
 friendlyColor = whoseTurn
--- projectedrange.hs
+++ projectedrange.hs
@@ -1,18 +1,18 @@
 module ProjectedRange ( ProjectedRange, project,) where
 
 import Control.Monad (liftM)
-import Control.Applicative ((<$>))
 import Data.Maybe (isNothing, isJust)
 import Data.List (findIndex, find)
 
 import MovingPiece ( MovingPiece, )
-import qualified Range ( series, position)
 import Square (Square, SquareSeries)
 import Range ( Range,)
-import Position ( Position, enemyColor, friendlyColor, enPassant,)
+import Position ( Position, enemyColor, friendlyColor, enPassant,
+                  isCapturableSquare, containsFriendlyPiece)
 import MoveType ( MoveType(..), movetypes)
 import Piece ( hasColor, PieceType(Pawn),)
 import Color ( Color, invert)
+import qualified Range ( series, position)
 
 data ProjectedRange = ProjectedRange { series :: [SquareSeries] }
 
@@ -24,13 +24,14 @@ project' r = map (projectSeries $ Range.position r) (Range.series r)
 
 projectSeries :: Position -> SquareSeries -> SquareSeries
 projectSeries p s = take ixFirstStop s
-    where ixFirstStop = maybe (length s) id (findFirstStop p s)
+    where ixFirstStop = maybe (length s) id (indexFirstStop p s)
 
-findFirstStop p s = min' enemyIx friendlyIx
-    where enemyIx = (+1) <$> findPieceColor (enemyColor p)
-          friendlyIx = findPieceColor (friendlyColor p)
-
-findPieceColor _ = Just 3
+indexFirstStop p s = min' enemyIx friendlyIx
+    where enemyIx = liftM (+1) (indexCapturable p s)
+          friendlyIx = indexFriendly p s
+          
+indexCapturable p s = findIndex (isCapturableSquare p) s
+indexFriendly p s = findIndex (containsFriendlyPiece p) s
 
 min' Nothing a = a
 min' a Nothing = a
