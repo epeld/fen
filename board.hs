1ea1fa06e0b85dc43c2f49f91b7cd594e2b84e6f
Erik Peldan <regnskog@gmail.com>
2013-06-01 11:45:53 +0200

    add state functionality

--- board.hs
+++ board.hs
@@ -1,4 +1,7 @@
 module Board where
+import Control.Arrow ((&&&))
+import Control.Monad.State (State, StateT(..))
+
 import Square ((!!!), Square)
 import Piece (Piece)
 
@@ -6,3 +9,28 @@ type Board = [Maybe Piece]
 
 readSquare :: Board -> Square -> Maybe Piece
 readSquare = (!!!)
+
+squareRead = flip readSquare
+
+-- TODO test this function
+replace :: Square -> Maybe Piece -> Board -> Board
+replace s mp b = let (part1, part2) = splitAt (fromEnum s) b
+                  in part1 ++ mp : drop 1 part2
+
+clear s = replace s Nothing
+
+type BoardState = State Board
+
+noSideEffect :: (b -> c) -> b -> ((), c)
+noSideEffect f = const () &&& f
+
+stateify = StateT . returnResult 
+    where returnResult = (return.)
+
+putMaybe s mp = stateify $ noSideEffect $ replace s mp
+
+put :: Square -> Piece -> BoardState ()
+put s p = putMaybe s (Just p)
+
+remove :: Square -> BoardState (Maybe Piece)
+remove s = stateify $ squareRead s  &&& clear s
