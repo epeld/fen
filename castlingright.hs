4ac2b8958c0b8647b57e679a0534c63dcbfb1035
Erik Peldan <regnskog@gmail.com>
2013-06-05 17:59:04 +0200

    fix spelling error, formatting

--- castlingright.hs
+++ castlingright.hs
@@ -1,8 +1,5 @@
-module CastlingRight (
-    Right(..),
-    Side(..)
-    ) where
+module CastlingRight ( Right(..), Side(..)) where
 import Color (Color)
 
 data Right = Castle Side Color deriving (Show, Eq)
-data Side = Kinside | Queenside deriving (Show, Eq)
+data Side = Kingside | Queenside deriving (Show, Eq)
