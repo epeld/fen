830c5aa24f9d427dc3f8a58406b4ba12815fe626
Erik Peldan <regnskog@gmail.com>
2013-07-13 14:26:06 +0200

    test indexes

--- testsquare.hs
+++ testsquare.hs
@@ -10,8 +10,21 @@ testList = TestList [
             TestLabel "horizontal series" $ TestCase testHorizontalSeries,
             TestLabel "file letters" $ TestCase testFileLetters,
             TestLabel "rank numbers" $ TestCase testRankNumbers,
-            TestCase testAbove, TestCase testBelow
+            TestLabel "squares" $ TestCase testSquares,
+            movementTests,
+            TestCase indexTest,
+            TestLabel "square indexes" $ indexesTest
             ]
+
+movementTests = TestList $ map TestCase $
+    [ testAbove, testBelow, testLeft, testRight ]
+
+indexTest = let s = square' 'e' 4
+             in assertEqual "index" (5 + 3*8) $ [1..] !!! s
+
+indexesTest = TestList $ map TestCase $ map assertValid indexes
+    where indexes = map ([1..] !!!) squares
+          assertValid = assertBool "< 64". (64 >=)
 
 squareTests = TestList [ TestCase testSquare, TestCase testSquareFail ]
 
@@ -28,8 +41,9 @@ testSquareFail =
      in do
         assertEqual "No square" Nothing $ s
 
+testSquares = assertLength 64 squares
 testFileLetters = assertLength 8 fileLetters
-testRankNumbers = assertLength 8 fileLetters
+testRankNumbers = assertLength 8 rankNumbers
 
 testHorizontalSeries = do
     let start = square' 'a' 3
@@ -47,6 +61,14 @@ testAbove = let s = square' 'e' 4
 testBelow = let s = square' 'e' 4
                 s' = square' 'e' 3
              in assertEqual "below" (Just s') (below s)
+
+testLeft = let s = square' 'e' 4
+               s' = square' 'd' 4
+             in assertEqual "left" (Just s') (left s)
+
+testRight = let s = square' 'e' 4
+                s' = square' 'f' 4
+             in assertEqual "right" (Just s') (right s)
 
 assertLength l a = assertEqual s l $ length a
     where s = "length " ++ show l
