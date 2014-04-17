0d4a2cddc0218c1498a0d3486aaadb2477dca576
Erik Peldan <epe@trifork.com>
2014-04-17 11:44:40 +0200

    Move HelperTest into new module, create QuickTests

--- ChessTest.hs
+++ ChessTest.hs
@@ -1,88 +1,29 @@
-module ChessTest where
+module HelpersTest where
 import Control.Monad.Reader (runReader)
 import Control.Applicative ((<*>), (<$>))
-import Test.HUnit.Base
-import Test.HUnit.Text
-import Data.Set (fromList)
+import Test.QuickCheck
+import Data.Map (fromList)
 import Data.Maybe (fromJust)
 import Types as Chess
 import qualified Fen
 import Chess
 
-predefined :: String -> Chess.Position
-predefined = fromJust. Fen.decode
+instance Arbitrary Chess.Square where
+  arbitrary = Chess.Square <$> choose ('a','h') <*> choose (1,8)
 
-startFen :: String
-startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+instance Arbitrary Chess.Piece where
+  arbitrary = Chess.Piece <$> arbitrary <*> arbitrary
 
-withFen fen = (`runReader` predefined fen)
+instance Arbitrary Chess.Color where
+  arbitrary = elements [Black, White]
 
+instance Arbitrary Chess.PieceType where
+  arbitrary = elements $ [Pawn] ++ (Officer <$> enumFromTo Knight King)
 
-tests = allUtilTests
+instance Arbitrary Chess.OfficerType where
+  arbitrary = oneof $ map return $ enumFromTo Knight King
 
-run = runTestTT tests
-
-
-allUtilTests = TestList $ utilTests ++ utilMiniTests
-
-
-utilTests = [lostCastlingTest, adjacentFilesTest]
-
-utilMiniTests = map TestCase utilAssertions
-utilAssertions = [assertEnemyColor, assertToggleWhite, assertToggleBlack]
-
-assertEnemyColor = assertEqual "enemy color" (withFen startFen enemyColor) Chess.Black
-assertToggleWhite = assertEqual "toggle white" Black (toggle White)
-assertToggleBlack = assertEqual "toggle black" White (toggle Black)
-
-e1 = Square 'e' 1
-e4 = Square 'e' 4
-e8 = Square 'e' 8
-
-h1 = Square 'h' 1
-h8 = Square 'h' 8
-
-a1 = Square 'a' 1
-a8 = Square 'a' 8
-
-f8 = Square 'f' 8
-
-adjacentFilesTest = TestCase $ do
-  assertAdjacent e4 f8
-  assertAdjacent f8 e4
-  assertBool "e1 e4 not adjacent files" (not (adjacentFiles e1 e8))
-
-assertAdjacent s1 s2 = do
-  let msg = (Fen.encode s1) ++ " " ++ (Fen.encode s2) ++ " have adjacent files"
-  assertBool msg (adjacentFiles s1 s2)
-
-lostCastlingTest = TestList [whiteKingMoveLosesBothRightsTest,
-                             whiteKingRookMoveLosesKingsideRightTest,
-                             whiteQueenRookMoveLosesQueensideRightTest,
-                             blackKingMoveLosesBothRightsTest,
-                             blackKingRookMoveLosesKingsideRightTest,
-                             blackQueenRookMoveLosesQueensideRightTest]
-
-whiteKingMoveLosesBothRightsTest =
-  testLoses e1 (Castling <$> [Queenside, Kingside] <*> [White])
-
-blackKingMoveLosesBothRightsTest =
-  testLoses e8 (Castling <$> [Queenside, Kingside] <*> [Black])
-
-whiteKingRookMoveLosesKingsideRightTest =
-    testLoses h1 (Castling <$> [Kingside] <*> [White])
-
-whiteQueenRookMoveLosesQueensideRightTest =
-    testLoses a1 (Castling <$> [Queenside] <*> [White])
-
-blackKingRookMoveLosesKingsideRightTest =
-    testLoses h8 (Castling <$> [Kingside] <*> [Black])
-
-blackQueenRookMoveLosesQueensideRightTest =
-    testLoses a8 (Castling <$> [Queenside] <*> [Black])
-
-testLoses :: Square -> [CastlingRight] -> Test
-testLoses sq rights = TestCase $ do
-  let lost = lostCastling sq
-  let msg = "moved from/to " ++ (Fen.encode sq)
-  assertEqual msg (fromList rights) lost
+arbitraryBoard :: Gen Chess.Board
+arbitraryBoard =
+  let kvs = zip <$> listOf1 arbitrary <*> listOf1 arbitrary
+  in fromList <$> kvs
