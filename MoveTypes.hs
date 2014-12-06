b0a99ddb1d700d1cfaac3e1761521d100095b1a9
Erik Peldan <epe@trifork.com>
2014-12-06 15:16:35 +0100

    Put move types in separate module

---
+++ MoveTypes.hs
@@ -0,0 +1,25 @@
+module MoveTypes where
+import Prelude ()
+import Data.Eq
+import Text.Show
+
+import Square
+
+
+data Move = Move deriving (Show, Eq)
+
+data MoveError = Ambiguous [SpecifiedMove] | Invalid deriving (Show, Eq)
+
+-- Specified Move: guarantees that there is a piece at its source square
+data SpecifiedMove = Specified Move Square deriving (Show, Eq)
+
+-- LegalMove: guarantees that the move is legal and results in a legal position
+data Legal a = Legal a
+type LegalMove = Legal SpecifiedMove
+
+-- SpecifiedPawnMove: guarantees that the moved piece is a pawn
+newtype SpecifiedPawnMove = PawnMove SpecifiedMove
+
+-- SpecifiedOfficerMove: guarantees that the moved piece is an officer
+newtype SpecifiedOfficerMove = OfficerMove SpecifiedMove
+
