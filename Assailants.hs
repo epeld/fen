6e13bd5342a597584b03be34d1a1c15929fd844c
Erik Peldan <epe@trifork.com>
2014-12-21 12:06:22 +0100

    Assailants can calculate which pieces can reach a square

--- Assailants.hs
+++ Assailants.hs
@@ -8,6 +8,7 @@ import Data.Maybe
 import Data.List
 import Data.Eq
 import Control.Monad.Reader
+import Control.Monad.Plus
 
 import PositionReader
 import qualified Position
@@ -16,25 +17,37 @@ import Square
 import Movement
 import SquareListUtils
 import Directions
+import PieceSquares
 
 
 allAssailants :: Color -> Square -> PReader [Square]
 
 allAssailants c sq = do
-    let as = assailants <$> allPiecesColored c <*> [sq]
-    mconcat `fmap` sequence as
+    as <- mapM (flip assailants sq) (allPiecesColored c)
+    return (mconcat as)
+
+allReachers c sq = do
+    oas <- mapM (flip assailants sq) (allOfficersColored c)
+    mp <- pawnReaching c sq
+    return (mconcat oas ++ mfromMaybe mp)
+
 
 
 assailants :: Piece -> Square -> PReader [Square]
 
-assailants pawn@(Piece Pawn c) sq = 
-    let ds = pawnAttackSources c
-     in filterM (hasPiece pawn) (concat $ apply1 sq ds)
+assailants pawn@(Piece Pawn c) sq = filterM (hasPiece pawn) (pawnAttackSquares c sq)
 
 assailants piece@(Piece (Officer officer) c) sq = do
     first <- mapM (firstPiece piece) (officerSquares officer sq)
     return (catMaybes first)
 
 
+pawnReaching :: Color -> Square -> PReader (Maybe Square)
+pawnReaching c sq = firstPiece pawn (pawnMoveSquares c sq)
+    where pawn = Piece Pawn c
 
+allPiecesColored :: Color -> [Piece]
 allPiecesColored c = Piece <$> [Pawn ..] <*> [c]
+
+allOfficersColored :: Color -> [Piece]
+allOfficersColored c = allPiecesColored c \\ [Piece Pawn c]
