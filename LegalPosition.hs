13788516e8b4a877f24596cdfca80c204807d0bf
Erik Peldan <epe@trifork.com>
2015-05-23 20:46:38 +0200

    Refactor code

--- LegalPosition.hs
+++ LegalPosition.hs
@@ -4,7 +4,9 @@ import Data.Eq
 import Data.Maybe
 import Data.Monoid
 import Data.Function
+import Data.Foldable
 import Data.List (filter)
+import Data.Int
 import Control.Applicative
 import Control.Monad.Plus
 import Control.Monad.Reader
@@ -19,46 +21,42 @@ import ListUtils
 data Error = MissingPromotion Square | KingAttacked Square
              deriving (Show, Eq)
 
-
 error :: PReader (Maybe Error)
 error = do
-    ka <- kingAttacked
-    mp <- missingPromotion
-    return $ getFirst $ ka `mappend` mp
+    ka <- kingAttackedErrors
+    mp <- missingPromotionErrors
+    return $ safeHead $ ka `mappend` mp
+    
 
+missingPromotionErrors :: PReader [Error]
+missingPromotionErrors = fmap MissingPromotion `liftM` missingPromotions
 
+kingAttackedErrors :: PReader [Error]
+kingAttackedErrors = fmap KingAttacked `liftM` kingAttackers
 
-missingPromotion :: PReader (First Error)
-missingPromotion = do
+missingPromotions :: PReader [Square]
+missingPromotions = do
     c <- turn
     lr <- lastRank
-    pawns <- filterPieces (Piece Pawn c)
 
-    let errors = filter (\sq -> rank sq == lr) pawns
-
-    return $
-        First $
-        case errors of
-            [] -> Nothing
-            (err:_) -> Just (MissingPromotion err)
+    pawns <- pawnSquares c
+    return $ mapMaybe (justRank lr) pawns
 
 
-
-kingAttacked :: PReader (First Error)
-kingAttacked = do
+kingAttackers :: PReader [Square]
+kingAttackers = do
     c <- turn
     pos <- ask
-    kingSqs <- kingSquares c
 
-    let attackers :: Square -> [Square]
-        attackers sq = runReader (allAssailants c sq) pos
-        sqs = mconcat $ fmap attackers kingSqs
+    let attackers sq = allAssailants c sq `runReader` pos
 
-    return $ 
-        First $ 
-        case sqs of
-            [] -> Nothing
-            (sq:_) -> Just (KingAttacked sq)
+    sqs <- kingSquares c
+    return $ concatMap attackers sqs
 
 
+pawnSquares :: Color -> PReader [Square]
+pawnSquares c = filterPieces (Piece Pawn c)
 
+
+justRank :: Int -> Square -> Maybe Square
+justRank r sq = if rank sq == r then Just sq else Nothing
