9ae55525873137d8bfec239ce443d1fa6b2057ab
Erik Peldan <epe@trifork.com>
2015-06-20 22:27:39 +0200

    Added position reader for asserting piece properties

--- test/TestUtils.hs
+++ test/TestUtils.hs
@@ -7,6 +7,18 @@ import Control.Monad.Reader
 import PositionReader
 import FENDecode
 
+hasPawnAt :: String -> PReader Spec
+hasPawnAt s = pieceSatisfyingAt (\pc -> pieceType pc == Pawn) s "has pawn"
+
+pieceSatisfyingAt :: (Piece -> Bool) -> String -> String -> PReader Spec
+pieceSatisfyingAt f s txt = do
+    pc <- pieceAt (square' s)
+    return $ 
+        describe ("square " ++ s) $ do
+            it (s ++ " isn't empty ") (assertJust pc) 
+            withJust' $ \p ->
+                it txt (f p)
+
 withInitialPosition :: PReader Spec -> Spec
 withInitialPosition = withPosition initialFEN
 
@@ -26,10 +38,16 @@ withRight' :: Either a b -> (b -> Spec) -> Spec
 withRight' (Right a) f = f a
 withRight' _ _ = doNothing
 
+withJust' :: Just a -> (a -> Spec) -> Spec
+withJust' (Just x) f = f x
+withJust' _ = doNothing
+
 assertRight x = x `shouldSatisfy` isRight
+assertJust x = x `shouldSatisfy` isJust
 
 isRight :: Either a b -> Bool
 isRight (Right _) = True
 isRight _ = False
+
 
 doNothing = return ()
