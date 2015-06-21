be00aa018595a472e6a31ce0cc6b8ffe1cae2300
Erik Peldan <epe@trifork.com>
2015-06-21 20:21:24 +0200

    Write test utils

--- test/HelperReaders.hs
+++ test/HelperReaders.hs
@@ -7,10 +7,31 @@ import TestUtils
 
 import Control.Monad.Reader
 import Data.Maybe
+import Data.Char (toLower)
 
 import Piece
 import Square
 import Position
+
+hasKnightAt :: Position -> String -> Spec
+hasKnightAt = hasOfficerAt Knight
+
+hasBishopAt :: Position -> String -> Spec
+hasBishopAt = hasOfficerAt Bishop
+
+hasKingAt :: Position -> String -> Spec
+hasKingAt = hasOfficerAt King
+
+hasRookAt :: Position -> String -> Spec
+hasRookAt = hasOfficerAt Rook
+
+hasQueenAt :: Position -> String -> Spec
+hasQueenAt = hasOfficerAt Queen
+
+hasOfficerAt :: OfficerType -> Position -> String -> Spec
+hasOfficerAt t p s = pieceSatisfyingAt p (\pc -> pieceType pc == Officer t) s label
+    where
+    label = unwords ["has", fmap toLower (show t)]
 
 hasPawnAt :: Position -> String -> Spec
 hasPawnAt p s = pieceSatisfyingAt p (\pc -> pieceType pc == Pawn) s "has pawn"
