29d88722d267788e926a287cadb1d06446dd4315
Erik Peldan <epe@trifork.com>
2015-07-04 22:01:40 +0200

    WIP unite standard- and castling moves

--- src/PGN.hs
+++ src/PGN.hs
@@ -1,11 +1,30 @@
 module PGN where
-import Text.Parsec
+import Control.Monad
 
-import PartialDescription
+import LegalMove
 
+data Error = Ambiguous [FullMove] | Invalid
 
-data PGNMove = PGNMove PartialDescription Meta deriving Show
+data Move = Standard PartialMove | Castles Side
 
-data Meta = Meta
+play :: Move -> PReader (Either Error Position)
+play (Castles s) = castle s
+play (Standard mv) = move mv
 
+-- foldr 'operand' to fold over a [Move] to produce a resulting position
+playr :: Move -> Either Error Position -> Either Error Position
+playr mv p = runReader (play mv) =<< p
 
+move :: PartialMove -> PReader (Either Error Position)
+move mv = do
+    mvs <- fullMoves mv
+    case mvs of
+        [x] -> Right <$> after x
+        [] -> return $ Left Invalid
+        xs -> return $ Left $ Ambiguous xs
+
+castle :: Side -> PReader (Either Error Position)
+castle s = 
+    if canCastle s
+    then Right <$> afterCastles s
+    else return $ Left Invalid
