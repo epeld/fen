46a9930da88b9e8a2a8284c3ccce7769de356442
Erik Peldan <epe@trifork.com>
2015-07-26 23:32:03 +0200

    Refactor legality-checking

--- src/LegalMove.hs
+++ src/LegalMove.hs
@@ -1,23 +1,74 @@
 module LegalMove where
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Except
 
--- TODO check that we can e.g capture on a square or move somewhere, and if not return error?
+
+data Error = NoCandidate | Ambiguous [FullMove] | InvalidMoveType MoveType derviving (Show, Eq)
+
+
+type LegalityFunction a = Position -> PartialMove -> Except (NonEmpty Error) a
+type Verifier :: LegalityFunction  ()
+
+
+fullMove :: LegalityFunction FullMove
+fullMove mv = do
+    mvs <- fullMoves mv
+    disambiguate mvs
 
 
 -- Figure out all the legal moves that can be made, given a partially specified move and a position
-fullMoves :: Qualifier src => Move src -> PReader [FullMove]
-fullMoves mv = do
-    cands <- candidates mv
-    let mvs = fullMove mv <$> cands
-    filterM isLegal mvs
+fullMoves :: LegalityFunction [FullMove]
+fullMoves p mv = do
+    verifyMoveType p mv
+
+    let legal = isLegal p
+        mvs = promote mv <$> cands
+        cands = candidates p mv
+
+    return (filter legal mvs)
 
 
+verifyMoveType :: Verifier
+verifyMoveType p mv = 
+    case mv ^. moveType of
+        Captures -> verifyStandardCapture p mv <|> verifyPassantCapture p mv
+        Moves -> verifyMove p mv
 
 
-isLegal :: FullMove -> PReader Bool
-isLegal mv = isNothing <$> error mv
+verifyStandardCapture :: Verifier
+verifyStandardCapture p mv =
+   if p ^. board . at (mv ^. destination) . color == Just (p ^. turn . to otherColor)
+   then pass
+   else throwE (InvalidMoveType Captures)
 
 
+verifyPassantCapture :: Verifier
+verifyPassantCapture p mv =
+    if p ^. board . at (mv ^. destination) == Nothing &&
+       p ^. passant == Just (mv ^. destination) &&
+       mv ^. moveType == Captures &&
+       mv ^. pieceType == Pawn
+    then pass
+    else throwE (InvalidMoveType Captures)
 
 
-error :: FullMove -> PReader (Maybe LP.Error)
-error mv = local (runReader $ after mv) LP.error
+verifyNoCapture :: Verifier
+verifyNoCapture p mv =
+    if p ^. board . at (mv ^. destination) == Nothing
+    then pass
+    else throwE (InvalidMoveType Moves)
+
+
+isLegal :: Position -> FullMove -> Bool
+isLegal p mv = isNothing (LegalPosition.error p')
+    where 
+    p' = after mv p
+
+
+disambiguate :: [FullMove] -> Except (NonEmpty Error) FullMove
+disambiguate [] = Left NoCandidates
+disambiguate [mv] = Right mv
+disambiguate mvs = Left $ Ambiguous mvs
+
+
+pass = return ()
