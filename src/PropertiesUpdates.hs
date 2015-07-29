d022e6e3848763ed97ee25a7ff6dbaf535975f3e
Erik Peldan <epe@trifork.com>
2015-07-29 21:54:15 +0200

    Rewrite properties updates completely

--- src/PropertiesUpdates.hs
+++ src/PropertiesUpdates.hs
@@ -1,83 +1,105 @@
 module PropertiesUpdates where
-import Prelude (undefined, succ)
-import Data.Eq
-import Data.Function
-import Data.Map as Map hiding (mapMaybe)
-import Data.Maybe
-import Data.Bool
-import Data.Monoid (mconcat)
-import Text.Show
-import Control.Monad
-import Control.Monad.Reader
+import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Data.Foldable
+import Data.Monoid
+import Control.Monad.Reader
+import Control.Applicative
+import Control.Lens
 
+import Castling
 import ListUtils
-import MoveDescription
 import Square
 import Piece
 import MoveType
-import PositionReader
-import qualified FullMove
-import UpdateFunctions
-import qualified Position
-import Castling
-import qualified PassantUtils
 import Move
+import FullMove
+import PawnMovement
+import PositionProperties as Properties
+
+type Update = Properties -> Properties 
+type UpdateReader = Reader (FullMove, Properties)
 
 
--- The properties stack contains all updateFns that will update the position's meta info (e.g move count etc)
-propertiesStack :: UpdateReader [UpdateFn]
-propertiesStack = sequence [fullMove, halfMove, castlingRights, passantSquare]
+-- Update properties using the information supplied in the FullMove
+properties :: UpdateReader Properties
+properties = do
+    (_, p) <- ask
+    compose <$> updates <*> pure p
+    
+
+updates :: UpdateReader [Update]
+updates = sequence [fullMove, halfMove, PropertiesUpdates.castlingRights, passantSquare]
 
 
-fullMove :: UpdateReader UpdateFn
+fullMove :: UpdateReader Update
 fullMove = do
-    color <- whoseMoveR
-    return $ case color of
-        Black -> \p -> p { Position.fullMoveCount = succ (Position.fullMoveCount p) }
+    (_, p) <- ask
+    return $ case p ^. Properties.turn of
+        Black -> fullMoveCount +~ 1
         White -> id
 
 
 -- "This is the number of halfmoves since the last capture or pawn advance. 
 --  This is used to determine if a draw can be claimed under the fifty-move rule."
-halfMove :: UpdateReader UpdateFn
+halfMove :: UpdateReader Update
 halfMove = do
-    mv <- moveR
-    let reset p = p { Position.halfMoveCount = 0 }
-        inc p = p  { Position.halfMoveCount = succ (Position.halfMoveCount p) }
+    (Full mv, _) <- ask
     return $ case mv of
-        PawnMove _ _ -> reset
-        OfficerMove _ (FullMove.Description _ _ Captures) -> reset
-        _ -> inc
+        PawnMove _ _ -> halfMoveCount .~ 0
+
+        OfficerMove _ _ ->
+            if mv ^. moveType == Captures
+            then halfMoveCount .~ 0
+            else halfMoveCount +~ 1
 
 
-castlingRights :: UpdateReader UpdateFn
+castlingRights :: UpdateReader Update
 castlingRights = do
-    mv <- moveR
-    let rights = Map.fromList $
-            [(sqr "a1", [Castling White Queenside]),
-             (sqr "h1", [Castling White Kingside]),
-             (sqr "a8", [Castling Black Queenside]),
-             (sqr "h8", [Castling Black Kingside]),
-             (sqr "e1", [Castling White Kingside, Castling White Queenside]),
-             (sqr "e8", [Castling Black Kingside, Castling Black Queenside])]
-        sqr = Square.unsafe
-        lost = Set.fromList $ mconcat $ mapMaybe (flip lookup rights) [FullMove.fullSource mv, destination mv]
-    return $ \p -> p { Position.castlingRights = Position.castlingRights p `Set.difference` lost}
+    (mv, _) <- ask
+    return $ Properties.castlingRights %~ (`Set.difference` lostCastlingRights mv)
+
 
 
 -- "En passant target square in algebraic notation. If there's no en passant target square, this is '-'. 
 --  If a pawn has just made a two-square move, this is the position "behind" the pawn. 
 --  This is recorded regardless of whether there is a pawn in position to make an en passant capture"
-passantSquare :: UpdateReader UpdateFn
+passantSquare :: UpdateReader Update
 passantSquare = do
-    psq <- PassantUtils.passantSquare
-    return $ \p ->
-        p { Position.passant = psq }
+    psq <- newPassantSquare
+    return $ passant .~ psq
 
 
-newTurn :: UpdateReader UpdateFn
-newTurn = do
-    color <- whoseMoveR
-    return $ \p -> p { Position.turn = otherColor color }
+newTurn :: UpdateReader Update
+newTurn = return $ turn %~ otherColor
 
+
+lostCastlingRights :: FullMove -> Set.Set CastlingRight
+lostCastlingRights (Full mv) = 
+    case foldMap (`Map.lookup` castlingRightsMap) sqs of
+        Nothing -> Set.empty
+        Just s -> s
+    where 
+    sqs = [mv ^. source, mv ^. destination]
+
+
+-- Map of which castling rights you lose if your move 'touches' a specific square (source or destination)
+castlingRightsMap :: Map.Map Square (Set.Set CastlingRight)
+castlingRightsMap = Map.map Set.fromList $ Map.mapKeys Square.unsafe $ Map.fromList pairs
+    where
+    pairs = [
+        ("a1", [Castling White Queenside]),
+        ("h1", [Castling White Kingside]),
+        ("a8", [Castling Black Queenside]),
+        ("h8", [Castling Black Kingside]),
+        ("e1", [Castling White Kingside, Castling White Queenside]),
+        ("e8", [Castling Black Kingside, Castling Black Queenside])]
+
+
+newPassantSquare :: UpdateReader (Maybe Square)
+newPassantSquare = do
+    (Full mv, p) <- ask
+    return $
+        case mv of
+            PawnMove _ _ -> behind (mv ^. destination) (p ^. turn)
+            _ -> Nothing
