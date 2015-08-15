358da23ebb4e9744bb36bdb131569f754abfa26d
Erik Peldan <epe@trifork.com>
2015-08-15 13:11:56 +0200

    white space

--- src/Square.hs
+++ src/Square.hs
@@ -4,25 +4,33 @@ import Control.Applicative
 import Data.List
 import Data.Char
 
+
 newtype Square = Square (Int, Int) deriving (Show, Eq, Ord)
 
+
 type Offset = (Int, Int)
+
 
 files :: [Char]
 files = ['a'..'h']
 
+
 ranks :: [Int]
 ranks = [1..8]
 
+
 string :: Square -> String
 string (Square (a, b)) = [chr (ord 'a' + a - 1), chr (ord '1' + b - 1)]
+
 
 square :: String -> Maybe Square
 square [file, rank] = digitToMaybeInt rank >>= square' file 
 square _ = Nothing
 
+
 digitToMaybeInt :: Char -> Maybe Int
 digitToMaybeInt c = if isDigit c then Just $ digitToInt c else Nothing
+
 
 square' :: Char -> Int -> Maybe Square
 square' f r = curry Square <$> fi <*> ri
@@ -36,6 +44,7 @@ unsafe s =
         Nothing -> error ("Invalid square string: '" ++ s ++ "'")
         Just sq -> sq
 
+
 add :: Square -> Offset -> Maybe Square
 add (Square (a, b)) (x, y) =
     let a' = a + x
@@ -44,15 +53,19 @@ add (Square (a, b)) (x, y) =
      in if elem a' [1..8] && elem b' [1..8]
         then Just $ Square (a', b')
         else Nothing
-                                 
+
+
 rank :: Square -> Int
 rank (Square (_, b)) = b
+
 
 file :: Square -> Int
 file (Square (a, _)) = a
 
+
 fileIndex :: Char -> Maybe Int
 fileIndex f = findIndex (== f) files
+
 
 diff :: Square -> Square -> (Int, Int)
 diff (Square (a,b)) (Square (x,y)) = (a - x, b - y)
